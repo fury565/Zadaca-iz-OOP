7ac65153e89894967bbc77172a8d265732c307a7
Bruno Licht <legotoster@gmail.com>
2020-10-22 16:22:47 +0200

    AV 1 Zadaca

---
+++ Zadaca1/Klase/Episode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Episode
+{
+    private int viewercount;
+    private double ratingsum;
+    private double maxrating;
+
+    public Episode() { }
+    public Episode(int viewercount, double ratingsum, double maxrating)
+    {
+        this.viewercount = viewercount;
+        this.ratingsum = ratingsum;
+        this.maxrating = maxrating;
+    }
+    public double GenerateRandomScore()
+    {
+        Random rnd = new Random();
+        double score = rnd.NextDouble() * 10;
+        return Math.Round(score,5);
+    }
+    public void AddView(double score)
+    {
+        viewercount++;
+        ratingsum += score;
+        if (maxrating < score)
+            maxrating = score;
+    }
+    public double GetAverageScore()
+    {
+        return ratingsum / viewercount;
+    }
+    public string GetMaxScore()
+    {
+        return maxrating.ToString();
+    }
+    public int GetViewerCount()
+    {
+        return viewercount;
+    }
+}
